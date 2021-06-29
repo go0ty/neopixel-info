19f58cb897493dcc22dfc62bf4a65406f39e798d
go0ty <true.early@gmail.com>
2021-06-30 00:41:11 +0100

    Fix Board

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 64, auto_wire=False)
-pixels.fill((255,0,0))
+pixels = neopixel.NeoPixel(board.D18, 64, auto_write=False, brightness=0.5)
+pixels.fill((0,0,0))
 pixels.show()
